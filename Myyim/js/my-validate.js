c5c2d8c4adfecf7a137dde5300ca61cb8f8b7f7f
sunsetgit <sunset_xinl@sina.com>
2014-11-28 12:06:17 +0800

    2
    2

--- Myyim/js/my-validate.js
+++ Myyim/js/my-validate.js
@@ -1,16 +1,14 @@
 $().ready(function() {
-	$("#registerForm").validate({
-		//debug: false,//只验证不提交
+	var registerValidator = $("#registerForm").validate({
+
 		rules: {
 			name: {
 				required: true,
-				isName: true, //自定义方法
-				minlength: true,
-				maxlength: true
+				minlength: 3,
+				maxlength: 6
 			},
 			psword: {
 				required: true,
-				isPsword: true
 			},
 			againPassword: {
 				required: true,
@@ -24,15 +22,13 @@ $().ready(function() {
 		messages: {
 			name: {
 				required: "请输入用户名",
-				isName: "只能输入字母,数字,横线,下划线",
-				minlength: true,
-				maxlength: true
+				minlength: "",
+				maxlength: ""
 			},
 			psword: {
 				required: "请输入您的密码",
-				isPsword: "只能输入字母,数字,横线,下划线",
-				minlength: true,
-				maxlength: true
+				minlength: "",
+				maxlength: ""
 			},
 			againPassword: {
 				required: "请再次输入您的密码",
@@ -61,40 +57,74 @@ $().ready(function() {
 			max: jQuery.validator.format("请输入一个最大为{0} 的值"),
 			min: jQuery.validator.format("请输入一个最小为{0} 的值")*/
 		},
+
+		//debug: true,//调试模式只验证不提交
+		onkeyup: false,
+		errorClass: "error",
+		validClass: "success",
+		errorElement: "span",
+		success: "success",
+
+		/*highlight: function(element, errorClass) {
+			$(element).addClass(errorClass).removeClass("success");
+		},
+		success: function(span) {
+			span.addClass("success");
+		},*/
 		//提交表单
 		submitHandler: function(form) {
 			alert("提交")
 				//form.submit();
 		},
+		//验证是否有错误信息
+		invalidHandler: function(form, validator) {
+			var errors = validator.numberOfInvalids();
+			if (errors) { //0表示false,非0表示true;
+				$("#message").text(validator.numberOfInvalids() + "error");
+				$("#message").show();
+			} else {
+				$("#message").hide();
+			}
+
+		},
 		//errorClass:"error",
 		//errorElement:"lable",
+
+		//将错误信息放到一个容器内
 		//errorContainer: "div.errorLable",
 		//errorLabelContainer: $("#registerForm div.errorLable"),
 		//wrapper: "li",
+
+		//打印错误个数
 		/*showErrors: function(errorMap, errorList) {
 			alert("您的表单中共有:" + this.numberOfInvalids() + "错误,请修正后提交!");
 			this.defaultShowErrors();
 		},*/
 
 	});
-	//自定义验证,addMethod(name,method,message),将name添加到rules中
-	//name也可以在html中以class的形式验证,若与rules中所验证的为同一输入框,
-	//则会以rules中对应的的提示信息展示,注意验证方式(提交验证,及时验证);
+	//自定义验证,addMethod(name,method,message),将name添加到对应表单中的class内;
 	//用户名称验证
 	jQuery.validator.addMethod("isName", function(value, element) {
 		var isName = /^[a-z0-9_-]{3,16}$/;
 		return this.optional(element) || (isName.test(value));
-	});
+	}, "只能输入字母,数字,横线,下划线");
 	//用户密码验证
 	jQuery.validator.addMethod("isPsword", function(value, element) {
 		var isPassword = /^[a-z0-9_-]{6,18}$/;
 		return this.optional(element) || (isPassword.test(value));
-	});
+	}, "只能输入字母,数字,横线,下划线");
 	//邮政编码验证
 	jQuery.validator.addMethod("isZipCode", function(value, element) {
 		//onkeyup: true; //及时验证
 		var tel = /^[0-9]{6}$/;
 		return this.optional(element) || (tel.test(value));
 	}, "请正确填写您的邮政编码");
-	
+	//检验是否通过验证,常结合invalidHandler使用
+	$(".isValid").on("focusout", function() {
+		$(this).parents("form").valid();
+		//$("#registerForm").valid();
+		return false;
+	});
+
+
 })
